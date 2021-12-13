daa583d950e95a91377121ece3e35ad1be21ff5f
6ernar667 <6ernar667@gmail.com>
2021-12-13 14:38:45 +0600

    sitev1

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# class Articles(models.Model):
+#     title1 = models.CharField('nazvanie1', max_length=50)
+#     title2 = models.CharField('nazvanie2', max_length=50)
+#     title3 = models.CharField('nazvanie3', max_length=50)
+#     title4 = models.CharField('nazvanie4', max_length=50)
+#     title5 = models.CharField('nazvanie5', max_length=50)
+# s
+#
+#     def __str__(self):
+#         return self.title
